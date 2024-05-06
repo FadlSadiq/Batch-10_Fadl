f0ee2538d58fee30c84abc71b5491014e5fc031f
SE-77\Batch 10 <fadilsadiq@gmail.com>
2024-05-06 14:09:39 +0700

    no message

---
+++ Project Pemula/Program.cs
@@ -0,0 +1,21 @@
+﻿class Cat
+{
+    string colour;
+    string species;
+    int age;
+
+    public void Eat()
+    {
+        Console.WriteLine("cat eat");
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Cat caty = new Cat();
+        caty.Eat();
+    }
+}
