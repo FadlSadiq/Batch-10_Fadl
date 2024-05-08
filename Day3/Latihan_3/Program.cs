47aac4177bc179d4e3423f9e89ed4057098cda6e
SE-77\Batch 10 <fadilsadiq@gmail.com>
2024-05-08 15:00:44 +0700

    Class Extension

---
+++ Day3/Latihan_3/Program.cs
@@ -0,0 +1,40 @@
+﻿public static class Calculator
+{
+    public static double Pi= 3.14;
+    public static double Add(double a, double b)
+    {
+        return a + b;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Calculator.Add(13.7, 23.5));
+        Console.WriteLine(Calculator.Pi);
+    
+    }
+}
+
+// public static class MethodExe
+// {
+//     public static string ToUpper(this string input);
+//     {
+//         if(string.IsNullOrEmpty(input))
+//         {
+//             return input;
+//         }
+//         return input.ToUpper();
+//     }
+// }
+
+// public class Program
+// {
+//     static void Main()
+//     {
+//         string msg = "Good Morning";
+//         Console.WriteLine(MethodeExe.ToUpper(msg));
+    
+//     }
+// }
