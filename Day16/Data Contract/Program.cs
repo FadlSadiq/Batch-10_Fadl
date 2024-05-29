1b70423ba1383ba13f92a8275a0a607a924aeb83
SE-77\Batch 10 <fadilsadiq@gmail.com>
2024-05-29 15:02:01 +0700

    SE

---
+++ Day16/Data Contract/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+[DataContract]
+
+class Player
+{
+    [DataMember]
+    private string _name;
+    [DataMember]
+    private int _money;
+    [DataMember]
+    public int Gold { get; set; }
+    [DataMember]
+    public int Exp { get; set; }
+
+    public Player(string name, int money, int gold, int exp)
+    {
+        _name = name;
+        _money = money;
+        Gold = gold;
+        Exp = exp;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+    public int GetMoney()
+    {
+        return _money;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Player juan = new Player("Juan", 2000, 300, 1500);
+        Player juna = new Player("juna", 4000, 400, 4500);
+        Player didi = new Player("didi", 5000, 500, 7500);
+
+        List<Player> player = new List<Player>()
+        {
+            juan, juna, didi
+        };
+
+        Dictionary<int, List<Player>> players = new Dictionary<int, List<Player>>()
+        {
+            {1, player}
+        };
+        
+        Dictionary<int, Player> players2 = new Dictionary<int, Player>()
+        {
+            {1, juan}, 
+            {2, juna}, 
+            {3, didi}
+        };
+
+        DataContractJsonSerializer serializer = new(typeof(Dictionary<int, List<Player>>));
+        DataContractJsonSerializer serializer2 = new(typeof(Dictionary<int, Player>));
+        using (FileStream fs = new("players.json", FileMode.Create))
+        {
+            serializer.WriteObject(fs,players);
+            serializer2.WriteObject(fs,players2);
+        }
+    }
+
+
+}
---
+++ Day16/StreamWriter/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program{
+    static void Main(){
+        using (var stream = new StreamWriter("text.txt"))
+        {
+            stream.WriteLine("Hello from the other side");
+        }
+//         // File name
+//         string fileName = "file.txt";
+
+//         FileManager fm = new();
+//         fm.Write(fileName, "Aku ingin liburan", 8);
+//         // System.Console.WriteLine(fm.ReadLine(fileName));
+//     }
+// }
+// class FileManager{
+//     public void Write(string path, string message, int number)
+//     {
+//         using (StreamWriter stream = new(path))
+//         {
+//             stream.WriteLine(message + " " + number);
+//         }
+//     }
+//     public string ReadLine(string path)
+//     {
+//         string result;
+//         using (StreamReader stream = new(path))
+//         {
+//             result = stream.ReadLine();
+//         }
+//         return result;
+    }
+}
