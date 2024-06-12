d8c8913b6da323bc2d790828c78593735c0b0cd4
LAPTOP-5KR1UO28\drwizz <fadilsadiq@gmail.com>
2024-06-12 08:15:26 +0700

    New Changes

---
+++ Day23/LampPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿public interface ILamp
+{
+    public void TurnOn();
+    public void TurnOff()
+}
+
+public class VendoraBAdapter : ILamp
+{
+    public VendoraBLight vendoraBLight;
+    public bool turn;
+
+    public void TurnOn
+}
+
+public class VendorALight : ILamp
+{
+public void TurnOn();
+public void TurnOff();
+}
+
+public class VendorBLight
+{
+    Public void TurnLight(bool on);
+}
